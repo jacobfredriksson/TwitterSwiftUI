51bf823277cf16209497c6be7fecef60c4ae3710
jacobfredriksson <jacobfredriksson@live.se>
2023-03-06 16:05:20 +0100

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Jacob Fredriksson on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
